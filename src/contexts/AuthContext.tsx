43d05abe5aca2807db068f67a61f64a142f9960f
YashPanwar1408 <panwarhp2351@gmail.com>
2025-04-28 21:56:37 +0530

    change url

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -80,7 +80,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
       const { error } = await supabase.auth.signInWithOAuth({
         provider: "google",
         options: {
-          redirectTo: 'https://auits-customercare-service.vercel.app/dashboard'
+          redirectTo: 'https://preview--auits.lovable.app/dashboard'
         }
       });
 
